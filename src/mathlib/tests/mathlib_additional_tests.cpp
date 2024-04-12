436c3467a99d83cfae75aaa9fc13c1b3fe0ec3de
Nikola Jordanov <njordanov06@gmail.com>
2024-04-12 21:25:46 +0200

    Changed the additional tests for the factorial function to accomodate for the change to uint64_t as it's output type Changed one test for the ln function to check for the result of 0 instead of -1

--- src/mathlib/tests/mathlib_additional_tests.cpp
+++ src/mathlib/tests/mathlib_additional_tests.cpp
@@ -12,24 +12,16 @@
 using namespace std;
 
 TEST(FACTORIAL, OF_Check){
-    unsigned long min_OF=1;//lowest whole number that tha factorial function cannot take as an input
-    long long int fact;	   //factrial of min_OF
-/*This loop increments the value of min_OF until it's factrial overflows
- *  or it exceeds the limits of ulong*/
-    for (fact=1; fact<=ULONG_MAX/min_OF; min_OF++){
-	if (fact<0) break;//if fact overflows the result is negative
-	fact=fact*min_OF;
-    }
-    for(long i=min_OF; i<100; i+=10){
-	EXPECT_ANY_THROW(Factorial(min_OF));
-    }
+    EXPECT_ANY_THROW(Factorial(21));
+    EXPECT_ANY_THROW(Factorial(22));
+    EXPECT_ANY_THROW(Factorial(100));
 }
 
 TEST(LN, functionality){
     EXPECT_EQ(ln(1),0);
     EXPECT_TRUE(ln(5)>=1.60943 && ln(5)<=1.60944);
     EXPECT_TRUE(ln(42)>=3.73766 && ln(42)<=3.73767);
-    EXPECT_ANY_THROW(ln(-1));
+    EXPECT_ANY_THROW(ln(0));
     EXPECT_ANY_THROW(ln(-5));
     EXPECT_ANY_THROW(ln(-42));
 }
