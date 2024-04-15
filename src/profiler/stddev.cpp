dc7fbf235c95e705b4f1aef9b447cb9216205c59
Fleck012 <alexzikla@gmail.com>
2024-04-15 13:44:16 +0200

    +Program to calculate sample standard deviations

---
+++ src/profiler/stddev.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include "../mathlib/include/mathlibrary.h"
+
+
+/**
+ * @param arr Vector of long double numbers
+ * @return Arithmetic mean
+*/
+long double CalculateMean(const std::vector<long double>& arr)
+{
+    long double sum = 0;
+    for (long double number : arr) {
+        sum = Add(sum, number);
+    }
+    return Div(sum, arr.size());
+}
+
+/**
+ * @param arr Vector of long double numbers
+ * @return Sample Standard Deviation
+*/
+long double CalculateSampleStandardDeviation(const std::vector<long double>& arr)
+{
+    long double mean = CalculateMean(arr);
+    long double deviation = 0;
+    for (long double number : arr) {
+        deviation = Add(deviation, Power(Sub(number, mean), 2) );
+    }
+    deviation = Div(deviation, arr.size());
+    return Root(deviation, 2);
+}
+
+int main()
+{
+    long double number;
+    std::vector<long double> arr;
+    std::cout << "Enter numbers: " << std::endl;
+    while (std::cin >> number) {
+        arr.push_back(number);
+    }
+
+    if (arr.empty()) {
+        std::cerr << "No numbers entered." << std::endl;
+        return 1;
+    }
+
+    long double deviation = CalculateSampleStandardDeviation(arr);
+    std::cout << "The standard deviation is: " << deviation << std::endl;
+
+    return 0; 
+}
