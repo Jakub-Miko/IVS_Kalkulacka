a775658a8b33b94e52b6dc630e93594ec2c5f6d3
Nikola Jordanov <njordanov06@gmail.com>
2024-04-19 19:45:33 +0200

    changed tests involving pi constant to account for floating point rounding errors

--- src/mathlib/tests/mathlib_goniomentry_test.cpp
+++ src/mathlib/tests/mathlib_goniomentry_test.cpp
@@ -11,24 +11,24 @@
 
 
 TEST(SIN, suite){
-    EXPECT_DOUBLE_EQ(Sine(0), 0);
-    EXPECT_DOUBLE_EQ(Sine(M_PI), 0);
+    EXPECT_TRUE(Sine(0)>= 0.00000 && Sine(0)<0.00001);
+    EXPECT_TRUE(Sine(M_PI)>=0.00000 && Sine(M_PI)<0.00001);
     EXPECT_TRUE(Sine(M_PI/2)>=1.00000 && Sine(M_PI/2)<1.00001);
     EXPECT_TRUE(Sine(3*M_PI/2)>=1.00000 && Sine(3*M_PI/2)<1.00001);
     EXPECT_TRUE(Sine(3)>=0.14112 && Sine(3)<0.14113);
 }
 
 TEST(COSINE, suite){
-    EXPECT_DOUBLE_EQ(Cosine(0), 1);
-    EXPECT_DOUBLE_EQ(Cosine(M_PI), -1);
-    EXPECT_DOUBLE_EQ(Cosine(M_PI/2), 0);
-    EXPECT_DOUBLE_EQ(Cosine(3*M_PI/2), 0);
+    EXPECT_TRUE(Cosine(0)>= 1.00000 && Cosine(0)<1.00001);
+    EXPECT_TRUE(Cosine(M_PI)<= -1.00000 && Cosine(M_PI)> -1.00001);
+    EXPECT_TRUE(Cosine(M_PI/2)>= 0.00000 && Cosine(M_PI/2)<0.00001);
+    EXPECT_TRUE(Cosine(3*M_PI/2)>= 0.00000 && Cosine(3*M_PI/2)<0.00001);
     EXPECT_TRUE(Cosine(2)<= -0.41614 && Cosine(2)> -0.41615);
 }
 
 TEST(TANGENT, suite){
-    EXPECT_DOUBLE_EQ(Tangent(0), 0);
-    EXPECT_DOUBLE_EQ(Tangent(M_PI), 0);
+    EXPECT_TRUE(Tangent(0)>= 0.00000 && Tangent(0)<0.00001 );
+    EXPECT_TRUE(Tangent(M_PI)>= 0.00000 && Tangent(M_PI)<0.00001);
     EXPECT_TRUE(Tangent(1)>=1.55740 && Tangent(1)<1.55741);
     EXPECT_TRUE(Tangent(2)<= -2.18503 && Tangent(2)> -2.18504);
 }
