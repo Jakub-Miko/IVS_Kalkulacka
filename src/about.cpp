fa56a0bc3606d18ae5c2d81803c89dd0dcd63aad
Jakub Miko <j.miko03@gmail.com>
2024-04-22 09:52:50 +0200

    Added font to resource file and added lincense to installation(temporary)

--- src/about.cpp
+++ src/about.cpp
@@ -1,5 +1,8 @@
 #include "about.h"
+#include "qdesktopservices.h"
 #include "ui_about.h"
+
+#include <QStandardPaths>
 
 about::about(QWidget *parent)
     : QDialog(parent)
@@ -12,3 +15,10 @@ about::~about()
 {
     delete ui;
 }
+
+void about::on_pushButton_clicked()
+{
+    QString file_path = QStandardPaths::locate(QStandardPaths::AppDataLocation, "LICENSE", QStandardPaths::LocateOption::LocateFile);
+    QDesktopServices::openUrl(QUrl::fromLocalFile(file_path));
+}
+
--- src/about.h
+++ src/about.h
@@ -15,6 +15,9 @@ public:
     explicit about(QWidget *parent = nullptr);
     ~about();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::about *ui;
 };
