465abc9b6fca9a118a3f11cc985c423936c09ac3
Jakub Miko <j.miko03@gmail.com>
2024-04-22 10:19:12 +0200

    Fixed fonts and added a way to package docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include <iostream>
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFontDatabase::addApplicationFont(":/resources/impact.ttf");
     MainWindow w;
     w.show();
     std::cerr << "Window Opened" << std::endl;
